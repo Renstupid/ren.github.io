443421eed1fe1c2ea50fa4b5150bf1e7ef597fb1
Rentenst <54294198+Renstupid@users.noreply.github.com>
2025-04-15 22:41:20 +0800

    Add files via upload

--- ren-web/script.js
+++ ren-web/script.js
@@ -1,5 +1,12 @@
 window.addEventListener('scroll', function () {
     const navbar = document.getElementById('navbar');
+    const btn = document.getElementById("hamburgerBtn");
+    const menu = document.querySelector(".nav-menu");
+
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
+
     if (window.scrollY > 10) {
       navbar.classList.add('scrolled');
     } else {
@@ -7,7 +14,13 @@ window.addEventListener('scroll', function () {
     }
   });
 
+  function goToGithib() {
+    window.open("https://github.com/Renstupid/", "_blank");
+  }
   function goToYoutube() {
     window.open("https://www.youtube.com/", "_blank");
   }
+
+  
+  
   
